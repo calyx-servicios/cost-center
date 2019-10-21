bd2063da1c80e7d1876282918c359faf56ad6734
Odzomek Jorge Matias <odzomek.matias@gmail.com>
2019-10-21 19:11:54 +0000

    [UPD] se agregan detalles en interfaz de debito y credito.-

--- cost_center/models/cost_center.py
+++ cost_center/models/cost_center.py
@@ -14,15 +14,19 @@ class CostCenter(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
     _rec_name = 'calculate_name'
 
+#### Fields
     name = fields.Char(string='Name', index=True, readonly=True, states={'draft': [('readonly', False)]})
     state = fields.Selection([
             ('draft','Draft'),
             ('open', 'Open'),
             ('archived', 'Archived')
             ], string='Status', index=True, readonly=True, default='draft',)
+
     company_id = fields.Many2one('res.company', string='Company', change_default=True,
         required=True, readonly=True, states={'draft': [('readonly', False)]},
         default=lambda self: self.env['res.company']._company_default_get('cost.center'))
+    currency_id = fields.Many2one(related="company_id.currency_id", string="Currency", readonly=True)
+
     description = fields.Char(string='Reference/Description', index=True, readonly=True, states={'draft': [('readonly', False)]}, copy=False,default='')
     analytic_id = fields.Many2one('account.analytic.account', string='Account Analytic', change_default=True, readonly=True, states={'draft': [('readonly', False)]})
     parent_id = fields.Many2one("cost.center", "Parent Center", select=True, states={'draft': [('readonly', False)]})
@@ -30,14 +34,29 @@ class CostCenter(models.Model):
 
     calculate_name = fields.Char('Name', compute='_get_calculate_name', store=True)
 
+    line_ids = fields.One2many('cost.center.move', 'cost_center_id', string='Lines',readonly=1 )
+    amount_debit = fields.Monetary(string='Total Debit', readonly=True, compute='_compute_amount')
+    amount_credit = fields.Monetary(string='Total Credit', readonly=True, compute='_compute_amount')
+#### end Fields
+    
+    @api.depends('line_ids.amount')
+    def _compute_amount(self):
+        amount_total = 0.0
+        for rec in self:
+            for line in rec.line_ids:
+                amount_total += line.amount
+
+        if amount_total>0.0:
+            amount_debit = amount_total
+            amount_credit = 0.0
+        else:
+            amount_debit = 0.0
+            amount_credit = amount_total
 
     @api.depends('name', 'parent_id.calculate_name')
     def _get_calculate_name(self):
         return_name = _('')
         for obj in self:
-            print('*****')
-            print(obj)
-            print('*****')
             if obj.parent_id:
                 return_name = _(obj.parent_id.calculate_name)  + _('/')+_(obj.name)
             else:
